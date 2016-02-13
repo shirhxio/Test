a0e4bb730ce067b128185826d7bf60a6701c8528
shirhxio <shirhxio537@gmail.com>
2016-02-13 18:49:47 +0900

    新しい方のはじめのUnityやってみました。

---
+++ TestUnity/Assets/Scripts/RollABall/Gamecontroller.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Gamecontroller : MonoBehaviour {
+	public UnityEngine.UI.Text scoreLabel;
+	public GameObject winnerLabelObject;
+
+	void Update () {
+		int count = GameObject.FindGameObjectsWithTag ("Item").Length;
+		scoreLabel.text = count.ToString ();
+
+		if (count == 0) {
+			winnerLabelObject.SetActive (true);
+		}
+	}
+}
---
+++ TestUnity/Assets/Scripts/TestInstantiate/Example.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Example : MonoBehaviour {
+	public Rigidbody projectile;
+	void Update() {
+		if (Input.GetButtonDown("Fire1")) {
+			Rigidbody clone;
+			clone = Instantiate(projectile, transform.position, transform.rotation) as Rigidbody;
+			clone.velocity = transform.TransformDirection(Vector3.forward * 10);
+			print (clone.name + ":" + clone.position);
+		}
+	}
+}
